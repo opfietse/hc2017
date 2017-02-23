00acdab4c2348cb02ce977f050c3e769ee1a69c3
Mark Reuvekamp <mreuvekamp1@wehkamp.nl>
2017-02-23 19:46:38 +0100

    test

--- src/main/java/com/wehkamp/hashcode/practise/Filereader.java
+++ src/main/java/com/wehkamp/hashcode/practise/Filereader.java
@@ -5,21 +5,23 @@ import java.io.*;
 public class Filereader {
     public Model read(String filename) {
         int lineCounter = 0;
-        int rows = 0;
-        int cols = 0;
-        int minIngredients = 0;
-        int maxSize = 0;
-        PizzaPart[][] pizzaParts = new PizzaPart[0][0];
+        int videos = 0;
+        int endpoints = 0;
+        int requests = 0;
+        int caches = 0;
+        int cacheSize = 0;
+//        PizzaPart[][] pizzaParts = new PizzaPart[0][0];
 
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (lineCounter == 0) {
                     String[] firstLineParts = line.split(" ");
-                    rows = Integer.parseInt(firstLineParts[0]);
-                    cols = Integer.parseInt(firstLineParts[1]);
-                    minIngredients = Integer.parseInt(firstLineParts[2]);
-                    maxSize = Integer.parseInt(firstLineParts[3]);
+                    videos = Integer.parseInt(firstLineParts[0]);
+                    endpoints = Integer.parseInt(firstLineParts[1]);
+                    requests = Integer.parseInt(firstLineParts[2]);
+                    caches = Integer.parseInt(firstLineParts[3]);
+                    cacheSize = Integer.parseInt(firstLineParts[4]);
                 } else {
                     //System.out.println(line);
 
